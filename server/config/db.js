47a35cbe14075dc67708896f18c319429a84fc3d
dev-SidD <sid25jun03@gmail.com>
2026-01-16 15:20:53 +0530

    Commit

--- server/config/db.js
+++ server/config/db.js
@@ -11,9 +11,9 @@ console.log(process.env.MYSQLPASSWORD)
 console.log(process.env.MYSQLDATABASE)
 const db = mysql.createConnection({
   host: process.env.MYSQLHOST || "mysql",
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
+  user: "root",
+  password: "nPVSLRVJyaoZWHTGOMuWpJNekPAgBdix",
+  database: "'railway'",
   port: process.env.MYSQLPORT || 3306,
 });
 

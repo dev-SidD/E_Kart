cac5d25afb250b27db7596ebbfef3a84c7802883
dev-SidD <sid25jun03@gmail.com>
2026-01-16 15:42:29 +0530

    Ready for frontend deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,18 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+  timeout: 10000,
 });
+
+API.interceptors.request.use(
+  (config) => config,
+  (error) => Promise.reject(error)
+);
+
+API.interceptors.response.use(
+  (response) => response,
+  (error) => Promise.reject(error)
+);
 
 export default API;

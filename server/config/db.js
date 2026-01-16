e8ba60b6201e5be32733ba8be528edb9c9e149ee
dev-SidD <sid25jun03@gmail.com>
2026-01-16 13:56:13 +0530

    Commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,5 @@
 import mysql from "mysql2";
-import dotenv from "dotenv";
 
-dotenv.config();
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST || "mysql",   // Railway internal service

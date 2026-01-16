862e95325c0befe4a5b152ea195e2546b5883e2e
dev-SidD <sid25jun03@gmail.com>
2026-01-16 15:15:20 +0530

    Commit

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,10 @@ import mysql from "mysql2";
  * when your backend service is connected to MySQL.
  * Do NOT use dotenv or dotenv-expand in production.
  */
-
+console.log(process.env.MYSQLHOST)
+console.log(process.env.MYSQLUSER)
+console.log(process.env.MYSQLPASSWORD)
+console.log(process.env.MYSQLDATABASE)
 const db = mysql.createConnection({
   host: process.env.MYSQLHOST || "mysql",
   user: process.env.MYSQLUSER,
